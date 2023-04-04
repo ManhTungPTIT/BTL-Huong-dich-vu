fb5bd4e5b6459d4391d33a1e3d8942ae77980e36
ManhTungPTIT <nguyentung191001@gmail.comm>
2023-04-04 14:59:31 +0700

    Update

--- js/cart.js
+++ js/cart.js
@@ -1,11 +1,27 @@
-var dem = 0;
-var page = document.getElementById("Total_checkbox");
-document.getElementById("checkbox").onclick = function (e) {
-  if (this.checked) {
-    page.innerHTML = "Tổng thanh toán(${dem + 1} sản phẩm)";
-    console.log(page);
-  } else {
-    page.innerHTML = "Tổng thanh toán(${dem} sản phẩm)";
-    console.log(page);
-  }
-};
+console.log("Js here");
+
+document.querySelectorAll("input.input-qty").forEach(function (input) {
+  var minus = input.parentNode.querySelector(".minus"),
+    plus = input.parentNode.querySelector(".plus"),
+    min = Number(input.getAttribute("min")),
+    max = Number(input.getAttribute("max")),
+    d = min === 0 ? 0 : min;
+  minus.addEventListener("click", function () {
+    console.log("sfdgsdgf");
+    if (this.classList.contains("minus")) {
+      if (d > min) d += -1;
+    } else if (this.classList.contains("plus")) {
+      var x = Number(input.value) + 1;
+      if (x <= max) d += 1;
+    }
+    input.setAttribute("value", d);
+    input.value = d;
+  });
+  plus.addEventListener("click", function () {
+    var x = Number(input.value) + 1;
+    if (x <= max) d += 1;
+    input.setAttribute("value", d);
+    input.value = d;
+  });
+  console.log("d", d);
+});
