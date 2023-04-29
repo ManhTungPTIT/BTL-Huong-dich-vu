c5740fb59778844d018dbb48071077d362fab831
ManhTungPTIT <nguyentung191001@gmail.comm>
2023-04-29 10:15:00 +0700

    Update

--- js/product.js
+++ js/product.js
@@ -1,7 +1,15 @@
-var Button_color = document.querySelectorAll(".color");
-var check_bt = false;
-for(var i = 0 ; i < Button_color.length ; i++){
-    
-}
+var myInput = document.getElementById("input_text");
+var editButton = document.getElementById("btEdit");
 
-
+editButton.addEventListener("click", function () {
+  var newValue = myInput.value;
+  var xhr = new XMLHttpRequest();
+  xhr.open("PUT", "http://127.0.0.1:5500/main/product.html");
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      alert("Data saved successfully");
+    }
+  };
+  xhr.send(JSON.stringify({ value: newValue }));
+});
